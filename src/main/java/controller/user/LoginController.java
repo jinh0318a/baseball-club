1209c112d28402e86803f5f81acbf2022bb26db7
jinh0318a <jinh0318a@naver.com>
2024-06-25 10:22:39 +0900

    add login

---
+++ src/main/java/controller/user/LoginController.java
@@ -0,0 +1,17 @@
+package controller.user;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class LoginController extends HttpServlet{
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/view/users/login.jsp").forward(request, response);
+	}
+}
